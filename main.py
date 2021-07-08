f85e96ecc0f1b5ad5e6912146f8750d1e9494cd9
Ravin Chowdhury <chowavc@gmail.com>
2021-07-08 21:59:07 +0530

    Multiple functions added

--- main.py
+++ main.py
@@ -1,20 +1,408 @@
 ## Importing useful packages
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import glob
+import os
+import shutil
+import pims
+import csv
+import trackpy as tp
 
 ## Importing useful scripts
 from utils import *
 
+
+
+## Remove all touching islands in each image
+def remove_touching_all(directory, tolerance=3):
+
+	# Reading output files
+	filesList = glob.glob(directory + 'labels/*.txt')
+
+	# If at least one file is found
+	if len(filesList) > 0:
+
+		# Folders to put outputted non-touching islands
+		folderNT = directory + 'labels_nt/'
+
+		# Make folder for non-touching
+		if len(glob.glob(folderNT)) == 1:
+			shutil.rmtree(folderNT)
+		os.mkdir(folderNT)
+
+		# Center of the bubble on image
+		bubble_center = [495, 618]
+
+		# List of points at the edge of the bubble
+		bubble_edges = [[495, 141], [21, 618], [947, 618], [110, 337], [826, 929], [291, 185]]
+
+		# Predict the radius of the bubble
+		radius = get_bubble_radius(bubble_center, bubble_edges)
+
+		# For every file in filesList
+		for i, labelFile in enumerate(filesList):
+
+			print('\nRemoving touching from file: ' + labelFile)
+
+			# Run remove touching
+			remove_touching_ff(labelFile, radius, tolerance=tolerance)
+
+
+
+## Create pandas dataframe for trackpy
+def to_pandas_df(directory, absolute=False, cutoff=0):
+
+	# Output dataframes
+	outPD_path = directory + 'dataframe.csv'
+
+	# Read all inputs
+	filesList = glob.glob(directory + 'labels_nt/*.txt')
+
+	# Output dictionary
+	outPD = {
+		'x': [],
+		'y': [],
+		'mass': [],
+		'size': [],
+		'ecc': [],
+		'signal': [],
+		'raw_mass': [],
+		'ep': [],
+		'frame': []
+	}
+
+	# As long as at least one file exists
+	if len(filesList) > 0:
+
+		# For every file
+		for i, labelFile in enumerate(filesList):
+
+			# Display current file
+			print("Doing file: " + labelFile)
+
+			if str(cutoff) in labelFile:
+
+				break
+
+			# Load label file as numpy array
+			labelMat = np.loadtxt(labelFile)
+
+			# For every island in array
+			for j, island in enumerate(labelMat):
+
+				# Format:
+				# calc_radius, calc_area, calc_phi, theta, lmbda, xc, yc, w, h
+				# 0 - calc_radius
+				# 1 - calc_area
+				# 2 - calc_phi
+				# 3 - theta
+				# 4 - lmbda
+				# 5 - xc
+				# 6 - yc
+				# 7 - w
+				# 8 - h
+
+				# If you want it in absolute (3d) coordinates
+				if absolute:
+
+					# This one is in absolute (3d) coordinates
+					outPD['x'].append(island[3]) # Theta
+					outPD['y'].append(island[4]) # Lambda
+					outPD['mass'].append(island[1])	# Area (absolute)
+					outPD['size'].append(island[1]) # Area (absolute)
+					outPD['ecc'].append(0.0) # Eccentricity (absolute, circles)
+					outPD['signal'].append(10.0) # ?
+					outPD['raw_mass'].append(island[1]) # Area (absolute)
+					outPD['ep'].append(0.1) # ?
+					outPD['frame'].append(i) # Frame number
+
+				# If you want the apparent (2d image) coordinates
+				else:
+
+					# This one is in absolute (3d) coordinates
+					outPD['x'].append(island[5]) # xc
+					outPD['y'].append(island[6]) # yc
+					outPD['mass'].append(island[1])	# Area (absolute)
+					outPD['size'].append(island[1]) # Area (absolute)
+					outPD['ecc'].append(0.0) # Eccentricity (absolute, circles)
+					outPD['signal'].append(10.0) # ?
+					outPD['raw_mass'].append(island[1]) # Area (absolute)
+					outPD['ep'].append(0.1) # ?
+					outPD['frame'].append(i) # Frame number
+
+
+
+	# Convert the dictionary to a pandas dataframe
+	outPD = pd.DataFrame.from_dict(outPD)
+
+	# Save the dataframe as csv
+	outPD.to_csv(outPD_path)
+
+
+
+## Link the islands from a csv pandas dataframe
+def link_islands(csv_file, dist, memory):
+
+	# Read unlinked csv file
+	unlinked = pd.read_csv(csv_file)
+
+	# Name for output linked dataframe
+	csv_out = csv_file.replace('dataframe', 'linked')
+
+	# Link the islands in the dataframe
+	linked = tp.link(unlinked, dist, memory=memo)
+
+	# Save the dataframe as csv
+	linked.to_csv(csv_out)
+
+
+
+## Display trajectories
+def disp_trajectories(csv_file):
+
+	# Read linked csv file
+	linked = pd.read_csv(csv_file)
+
+	# Plot trajectories
+	tp.plot_traj(linked);
+
+
+
+## Plot velocity field
+def plot_velo(inDir, csv_file, frame=1000, delta=5):
+
+	# Read linked csv file
+	linked = pd.read_csv(csv_file)
+
+	# Path to frame
+	framePath = inDir + 'frames/' + str(frame).zfill(4) + '.tif'
+
+	# Image read
+	im = Image.open(framePath)
+
+	# Store matrix of values for current frame
+	labelMatCurrent = []
+	# Store matrix of values for frame + delta
+	labelMatDelta = []
+
+	# Store plot lists in this dictionary
+	toPlot = {
+		'x': [],
+		'y': [],
+		'dx': [],
+		'dy': []
+	}
+
+	# Open linked csv file
+	with open(csv_file, 'r') as data:
+
+		# Go through it linewise
+		for i, line in enumerate(csv.reader(data)):
+
+			# If it is from the right frame
+			if line[-2] == str(frame):
+
+				# Add the line
+				labelMatCurrent.append(np.asarray(line))
+
+			# If it is from frame + delta
+			if line[-2] == str(frame + delta):
+
+				# Add the line
+				labelMatDelta.append(np.asarray(line))
+
+	# For each line in the current frame
+	for l1 in labelMatCurrent:
+
+		# Extract particle number
+		currentPart = l1[-1]
+
+		# Try to find the same particle in the frame+delta
+		for l2 in labelMatDelta:
+
+			# If it is found
+			if l2[-1] == currentPart:
+
+				# Store coordinates in frame
+				toPlot['x'].append(int(float(l1[2])))
+				toPlot['y'].append(int(float(l1[3])))
+
+				# Store relative motion
+				toPlot['dx'].append(int(float(l2[2])) - int(float(l1[2])))
+				toPlot['dy'].append(int(float(l2[3])) - int(float(l1[3])))
+
+	# Plot the frame image
+	plt.imshow(im)
+
+	# Plot the velocity field
+	plt.quiver(toPlot['x'], toPlot['y'], toPlot['dx'], toPlot['dy'], pivot='middle', linewidths=1, headwidth=4, headlength=6, color='red')
+	
+	# Turn off axis
+	plt.axis('off')
+
+	# Save the figure with no white space
+	plt.savefig(csv_file.replace('csv', 'tif').replace('linked', 'velo'), bbox_inches = 'tight', pad_inches=0, dpi=400)
+
+	# Show the figure
+	plt.show()
+
+
+
+## Plot diameters changing over time
+def plot_diam(csv_file, minLen=0, diamRange=[0, 100], plotSingle=True):
+
+	# Center of the bubble on image
+	bubble_center = [495, 618]
+
+	# List of points at the edge of the bubble
+	bubble_edges = [[495, 141], [21, 618], [947, 618], [110, 337], [826, 929], [291, 185]]
+
+	# Predict the radius of the bubble
+	radius = get_bubble_radius(bubble_center, bubble_edges)
+
+	# Output plot name
+	plot_path = csv_file.replace('csv', 'tif').replace('linked', 'diameters')
+
+	# Read linked csv file
+	linked = pd.read_csv(csv_file)
+
+	# Dictionary to store what has to be displayed
+	dictPlot = {}
+
+	# Open linked csv file
+	with open(csv_file, 'r') as data:
+
+		# Go through it linewise
+		for i, line in enumerate(csv.reader(data)):
+
+			if i%10000 == 0:
+
+				print('Currently on line: ' + str(i))
+
+			# The first line contains the headers (keys)
+			if i != 0:
+
+				# This is which particle it is
+				particle = line[-1]
+
+				# Add particle key to dictionary if it does not exist
+				if particle not in dictPlot.keys():
+
+					# Adding empty list
+					dictPlot[particle] = []
+
+				coords = spherical(bubble_center, radius, (float(line[2]), float(line[3])))
+			
+				# Current latitude (this is the x-coord)
+				cLat = coords[1]*180/np.pi
+
+				# Diameter
+				cD = 2*np.sqrt((float(line[4])/np.pi))
+
+				# Only if the Diameter is in the range we want to view
+				if cD >= diamRange[0] and cD <= diamRange[1]:
+
+					# Add the coordinates to the dictionary
+					dictPlot[particle].append([cLat, cD])
+
+	# Clear plots
+	plt.clf()
+
+	# Store keys that must be removed
+	to_remove = []
+
+	# Display how many islands are there in total
+
+
+	# Plot every island
+	for key in dictPlot.keys():
+
+		# Extract the island information from dictionary
+		currentPart = np.asarray(dictPlot[key])
+
+		# Transpose it
+		currentPart = currentPart.T
+
+		# If it is not long enough in frames, remove it
+		if not (len(currentPart) > 0 and len(currentPart[0]) > minLen):
+
+			# Add key to remove
+			to_remove.append(key)
+
+		# If you want to plot multiple islands
+		elif not plotSingle:
+
+			# Plot island
+			plt.plot(currentPart[0], currentPart[1])
+
+	# Remove keys from dict
+	for key in to_remove:
+
+		# Remove from dictionary
+		dictPlot.pop(key)
+
+	# If you wanted to plot a single island
+	if plotSingle:
+
+		# Keep running again and again
+		while True:
+
+			# Display the keys
+			print()
+			print(dictPlot.keys())
+
+			# Get input of what island to view
+			currentKey = input("Enter the island you want to view ('exit' to leave): ")
+
+			# If user wants to exit, do so
+			if currentKey == 'exit':
+				return
+
+			# Display requested island
+			currentPart = np.asarray(dictPlot[currentKey])
+			currentPart = currentPart.T
+
+			# Clear plot
+			plt.clf()
+			plt.plot(currentPart[0], currentPart[1])
+
+			plt.title('Island diameter vs latitude')
+			plt.xlabel('Lat (degrees)')
+			plt.ylabel('Diameter (pixels)')
+			# Show plot
+			plt.show()
+
+	plt.title('Island diameter vs latitude')
+	plt.xlabel('Lat (degrees?)')
+	plt.ylabel('Diameter (pixels)')
+	# Show cumulative plot
+	plt.show()
+
+
 ## Main function of the file
-def main(directory, display=False):
+def main(directory, display=False, showIsland=False):
 
 	# Reading input label files
 	filesList = glob.glob(directory + 'labels/*.txt')
 
 	# If at least one file found
 	if len(filesList) > 0:
+
+		# Make folder for outputs
+		if len(glob.glob('output/')) == 0:
+			os.mkdir('output/')
+
+		# Output folder
+		outDir = directory.replace('input', 'output')
+
+		# Make folder for specific run
+		if len(glob.glob(outDir)) == 1:
+			shutil.rmtree(outDir)
+		os.mkdir(outDir)
+		os.mkdir(outDir + 'labels/')
+		os.mkdir(outDir + 'frames/')
 
 		# Dimensions of the image
 		dims = (1024, 1024)
@@ -23,7 +411,7 @@ def main(directory, display=False):
 		bubble_center = [495, 618]
 
 		# List of points at the edge of the bubble
-		bubble_edges = [[495, 141], [21, 618], [947, 618]]
+		bubble_edges = [[495, 141], [21, 618], [947, 618], [110, 337], [826, 929], [291, 185]]
 
 		# Predict the radius of the bubble
 		radius = get_bubble_radius(bubble_center, bubble_edges)
@@ -34,16 +422,91 @@ def main(directory, display=False):
 			print("\nDoing " + labelFile + ', ' + str(i+1) + '/' + str(len(filesList)))
 
 			# Calculate predictions for radii of each island
-			island_sizes(labelFile, dims, bubble_center, radius, display=display)
+			island_sizes(labelFile, dims, bubble_center, radius, display=display, showIsland=showIsland)
 
 
 
 ## If file is run directly
 if __name__ == '__main__':
 
+	####
+	# Main function
 	# Input directory
-	inDir = 'input/309_tm_25C_top_need_35C/'
+	# inDir = 'input/309_tm_25C_top_need_35C/'
 	# inDir = 'input/309_tm_26C_bot_need_40C/'
 
 	# Run main function
-	main(inDir, False)
+	# main(inDir, False, False)
+
+	####
+	# Remove touching from output
+	# touchDir = 'output/309_tm_25C_top_need_35C/'
+	# touchDir = 'output/309_tm_26C_bot_need_40C/'
+
+	# Tolerance for touching
+	tol = 3
+
+	# Run remove touching function
+	# remove_touching_all(touchDir, tol)
+
+	####
+	# Saving pandas dataframe for trackpy
+
+	# Directory
+	# outdir = 'output/309_tm_25C_top_need_35C/'
+	# outdir = 'output/309_tm_26C_bot_need_40C/'
+
+	absolute = False
+
+	# Run function to make dataframe
+	# to_pandas_df(outdir, absolute, cutoff=1233)
+
+	####
+	# Linking islands
+
+	# Path to pandas dataframe
+	# pathPD = 'output/309_tm_25C_top_need_35C/dataframe.csv'
+	# pathPD = 'output/309_tm_26C_bot_need_40C/dataframe.csv'
+
+	# Run functions to link islands
+	dis = 5
+	memo = 3
+
+	# link_islands(pathPD, dist=dis, memory=memo)
+
+	####
+	# Displaying trajectories
+
+	# Path to linked pandas dataframe
+	# pathPD_linked = 'output/309_tm_25C_top_need_35C/linked.csv'
+	# pathPD_linked = 'output/309_tm_26C_bot_need_40C/linked.csv'
+
+	# Call function
+	# disp_trajectories(pathPD_linked)
+
+	####
+	# Plotting instantaneous velocity
+
+	# Path to input directory
+	# inDir = 'input/309_tm_25C_top_need_35C/'
+	# inDir = 'input/309_tm_26C_bot_need_40C/'
+
+	# Path to linked pandas dataframe
+	# pathPD_linked = 'output/309_tm_25C_top_need_35C/linked.csv'
+	# pathPD_linked = 'output/309_tm_26C_bot_need_40C/linked.csv'
+
+	frame = 100
+	delta = 50
+
+	# Call function
+	# plot_velo(inDir, pathPD_linked, frame, delta)
+
+	####
+	# Plotting change in diameter
+
+	# Path to linked pandas dataframe
+	pathPD_linked = 'output/309_tm_25C_top_need_35C/linked.csv'
+	# pathPD_linked = 'output/309_tm_26C_bot_need_40C/linked.csv'
+
+	# Call function
+	plot_diam(pathPD_linked, minLen=100, diamRange=[0, 500], plotSingle=True)
--- utils.py
+++ utils.py
@@ -5,6 +5,96 @@ import matplotlib.patches as ps
 from PIL import Image
 
 
+## Remove touching islands from label file
+def remove_touching_ff(label_file, globe_radius, tolerance=3):
+
+	# Output file
+	output_file = label_file.replace('labels', 'labels_nt')
+
+	# Reading label_file
+	labelMat = np.loadtxt(label_file)
+
+	# Storing flagged touching islands
+	flagged = []
+
+	# For every island
+	for i, i1 in enumerate(labelMat):
+
+		# Choose another island
+		for j, i2 in enumerate(labelMat):
+
+			# They cannot be the same
+			if i != j:
+
+				# Read i1 useful data
+				r1 = i1[0] # Radius
+				c1 = [i1[3], i1[4]] # Spherical coords
+
+				# Read i2 useful data
+				r2 = i2[0] # Radius
+				c2 = [i2[3], i2[4]] # Spherical coords
+
+				# Get distance between islands
+				dist = distance_on_globe(globe_radius, c1, c2)
+
+				# Check if dist is lesser than sum of radii plus tolerance
+				if dist < r1 + r2 + tolerance:
+
+					# If not already flagged
+					if i not in flagged:
+						flagged.append(i)
+
+					# If not already flagged
+					if j not in flagged:
+						flagged.append(j)
+
+	# Storing output mat
+	out_array = []
+
+	# Now copy over all elements of label matrix, unless island is flagged
+	for i, island in enumerate(labelMat):
+
+		# If not flagged
+		if i not in flagged:
+
+			# Append island
+			out_array.append(island)
+
+	# Save the output
+	np.savetxt(output_file, np.asarray(out_array))
+
+
+## Find the distance between two points on the surface of a globe
+def distance_on_globe(globe_radius, c1, c2):
+
+	# c1 and c2 are (theta, lambda)
+
+	# Convert thetas to longitude (the same)
+	lon1 = c1[0]
+	lon2 = c2[0]
+
+	# Convert lambdas to latitude
+	lat1 = c1[1] - np.pi/2
+	lat2 = c2[1] - np.pi/2
+
+	# Angular difference
+	dlon = lon2 - lon1
+	dlat = lat2 - lat1
+
+	# Haversin function
+	a = np.sin(dlat/2)**2 + np.cos(lat1)*np.cos(lat2)*np.sin(dlon/2)**2
+
+	# Finding angular distance
+	c = 2*np.arcsin(np.sqrt(a))
+
+	# Great circle distance
+	d = globe_radius*c
+
+	# Return result
+	return d
+
+
+
 ## Calculate radius of the bubble from the position of the center, and a list of points at the edge of the bubble
 def get_bubble_radius(bubble_center, bubble_edges):
 
@@ -91,14 +181,39 @@ def island_radius(globe_center, globe_radius, bb_center, bb_dims):
 	return radius, phi, dr
 
 
+
+## Get spherical coordinates
+def spherical(globe_center, globe_radius, island_center):
+
+	# Location of center of island with respect to the center of the bubble
+	dx = globe_center[0] - island_center[0]
+	dy = globe_center[1] - island_center[1]
+
+	# Angle around the left and right
+	theta = np.arcsin(dx/globe_radius)
+
+	# Angle from top to bottom
+	lmbda = np.arccos(dy/globe_radius)
+
+	# Return resultant coordinates
+	return theta, lmbda
+
+
+
 ## Label all island radii and sizes with a YOLOv5 label file
 def island_sizes(label_file, dims, bubble_center, bubble_radius, display=False, showIsland=False):
 
 	# Generate ID for output file
-	output_file = 'output/' + label_file.split('/')[-2] + '/' + label_file.split('/')[-1]
+	output_file = label_file.replace('input', 'output')
+
+	# Generate ID for output image
+	output_image = output_file.replace('labels', 'frames').replace('txt', 'png')
 
 	# Load labels from input file
 	labelMat = np.loadtxt(label_file)
+
+	# Output matrix of islands
+	out_array = []
 
 	# Image displaying island path
 	imagePath = label_file.replace('labels', 'frames').replace('txt', 'tif')
@@ -109,16 +224,10 @@ def island_sizes(label_file, dims, bubble_center, bubble_radius, display=False, 
 	# Clear plots
 	plt.clf()
 
-	# First subplot
-	plt.subplot(1, 2, 1)
+	# Title the plot
+	plt.title('Image with overlaid islands')
 
-	# Displaying original image
-	plt.imshow(image)
-	plt.title('Original Image')
-
-	# Second subplot
-	plt.subplot(1, 2, 2)
-	plt.title('Image with overlaid island')
+	# Show image
 	plt.imshow(image)
 	ax = plt.gca()
 
@@ -128,23 +237,37 @@ def island_sizes(label_file, dims, bubble_center, bubble_radius, display=False, 
 		# Get the absolute pixelwise 
 		xc, yc, w, h = label_to_bb(det, dims)
 
+		# Center of ellipse on the image
+		island_center = (xc, yc)
+
 		# Predict the radius of the island, get angle and distance from center
-		calc_radius, calc_phi, calc_dist = island_radius(bubble_center, bubble_radius, (xc, yc), (w, h))
+		calc_radius, calc_phi, calc_dist = island_radius(bubble_center, bubble_radius, island_center, (w, h))
+
+		# Spherical coordinates of center of island
+		theta, lmbda = spherical(bubble_center, bubble_radius, island_center)
 
 		# Calculate area from predicted area
 		calc_area = np.pi*calc_radius**2
+
+		# Append the island info to array
+		if calc_radius != 0:
+			out_array.append(np.array([calc_radius, calc_area, calc_phi, theta, lmbda, xc, yc, w, h]))
 
 		# Display results
 		if display:
 			print("\nIsland " + str(i+1) + '/' + str(len(labelMat)))
 			print('Predicted island center: (' + str(xc) + ', ' + str(yc) + ')')
+			print("Predicted island center spherical (\u03B8, \u03BB): (" + str(theta*(180/np.pi))[:5] + "\u00B0, " + str(lmbda*(180/np.pi))[:5] + "\u00B0)")
 			print("Predicted island radius: " + str(calc_radius)[:5] + " px")
 			print("Predicted island area: " + str(calc_area)[:7] + " sq. px")
 
-		island_center = (xc, yc)
+		# Width of the ellipse (twice semiminor axis)
 		ell_width = 2*calc_radius*np.cos(np.arcsin(calc_dist/bubble_radius))
+
+		# Height of the ellipse (twice semimajor axis)
 		ell_height = 2*calc_radius
 
+		# Add the ellipse
 		ax.add_patch(ps.Ellipse(island_center,
 			width=ell_width,
 			height=ell_height,
@@ -153,8 +276,20 @@ def island_sizes(label_file, dims, bubble_center, bubble_radius, display=False, 
 			facecolor='none',
 			linewidth=1))
 
-	if show_island:
+	# Save the numpy array
+	np.savetxt(output_file, np.asarray(out_array))
+
+	# Save the figure
+	plt.savefig(output_image, dpi=300)
+
+	# If you want to display
+	if showIsland:
+
+		# Show the figure
 		plt.show()
+
+	# Clear figure
+	plt.clf()
 
 
 ## Running this if the script is run directly
@@ -170,10 +305,10 @@ if __name__ == '__main__':
 	bubble_center = [495, 618]
 
 	# List of points at the edge of the bubble
-	bubble_edges = [[495, 141], [21, 618], [947, 618]]
+	bubble_edges = [[495, 141], [21, 618], [947, 618], [110, 337], [826, 929], [291, 185]]
 
 	# Predict the radius of the bubble
 	radius = get_bubble_radius(bubble_center, bubble_edges)
 
 	# Calculate predictions for radii of each island
-	island_sizes(file, dims, bubble_center, radius, display=True, showIsland=True)
+	island_sizes(file, dims, bubble_center, radius, display=True, showIsland=False)
